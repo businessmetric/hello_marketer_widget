9758637c44cc95055b83bf26a7c6609be2a9b088
Alexander Gorbatov <57249660+businessmetric@users.noreply.github.com>
2026-01-13 22:55:48 +0300

    Update widget.js

--- widget.js
+++ widget.js
@@ -6,6 +6,6 @@ Live operator video widget for websites.
 
 ```html
 <script>
-  window.HELLO_MARKETER_WS = 'wss://your-server.com';
+  window.HELLO_MARKETER_WS = 'wss://ideal-dollop-5rrvx7w7pq7frpq-8081.app.github.dev';
 </script>
 <script src="https://cdn.jsdelivr.net/gh/businessmetric/hello_marketer_widget@main/widget.js"></script>
